df7ef8969751ad200d26ea02e1b098f20baea3f0
NikkiHmltn <nikkihmltn1@yahoo.com>
2022-04-30 19:06:52 -0700

    minor fix

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
-const e = require('express');
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cors())
 
 const routes = require('./routes')
 const apiCheck = require('./middleware/checkKey')
