b5a5130d512e94dac7728bab03c8b2458f14366c
NikkiHmltn <nikkihmltn1@yahoo.com>
2023-02-06 15:15:42 -0800

    test

--- controllers/colorCtrl.js
+++ controllers/colorCtrl.js
@@ -3,7 +3,7 @@ const parseString = require('xml2js').parseString
 
 
 const getPalette = (req, res) => {
-    axios.get('http://www.colourlovers.com/api/palettes/random')
+    axios.get('http://www.colourlovers.com/api/palettes/random?format=json')
     .then((response) => {
         console.log(response)
         parseString(response.data, (err, results) => {
