9604217e58049bf848a1d5cc08a544c0fcf26ba4
NikkiHmltn <nikkihmltn1@yahoo.com>
2023-02-06 14:48:50 -0800

    added some org notes

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 
+/* ======== MIDDLEWARE ========= */
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors({origin: process.env.HEROKU_URL}))
 
+/* ======== INTERNAL MIDDLEWARE ========= */
 const routes = require('./routes')
 const apiCheck = require('./middleware/checkKey')
 
+/* ======== ROUTES ======== */
 app.use("/test", (req, res) =>{
     res.send("Test")
 })
@@ -20,6 +23,7 @@ app.use((req,res) => {
     res.status(404).json({message: "Not a proper route."})
 })
 
+/* ======= LISTENER ======= */
 app.listen(process.env.PORT, (req, res) =>{
     console.log(`Server running on port:${process.env.PORT}`)
 })
