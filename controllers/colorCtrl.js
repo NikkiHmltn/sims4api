6106172c61573c49f49426e113c2d0decac278fb
NikkiHmltn <nikkihmltn1@yahoo.com>
2023-02-06 17:33:11 -0800

    push

--- controllers/colorCtrl.js
+++ controllers/colorCtrl.js
@@ -1,24 +1,8 @@
-const axios = require('axios')
-const parseString = require('xml2js').parseString
-
-const options = {
-    headers: {
-        'Access-Control-Allow-Origin': '*'
-    }
-}
+const colorPalette = require('nice-color-palettes/1000')
 
 const getPalette = (req, res) => {
-    axios.get('http://www.colourlovers.com/api/palettes/random?format=json', options)
-    .then((response) => {
-        console.log(response)
-        parseString(response.data, (err, results) => {
-            res.status(200).json({
-                msg: "Palette retrieved", 
-                imageUrl: results.palettes.palette[0].imageUrl
-            })
-        })
-    })
-    .catch((err) => console.log(err))
+    const randomIdx = Math.floor(Math.random() * colorPalette.length)
+    return colorPalette[randomIdx]
 }
 
 module.exports = {
