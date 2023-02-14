a2dfd78a2020878ab02b031c622abba3f9f5f2a6
NikkiHmltn <nikkihmltn1@yahoo.com>
2023-02-14 14:53:15 -0800

    got rid of API for local dep for color palettes

--- controllers/colorCtrl.js
+++ controllers/colorCtrl.js
@@ -1,8 +1,10 @@
 const colorPalette = require('nice-color-palettes/1000')
 
 const getPalette = (req, res) => {
+
     const randomIdx = Math.floor(Math.random() * colorPalette.length)
-    return colorPalette[randomIdx]
+    console.log(colorPalette[randomIdx])
+    return res.status(200).json({data: colorPalette[randomIdx]})
 }
 
 module.exports = {
